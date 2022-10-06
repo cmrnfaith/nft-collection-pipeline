590a52a325093eec6a70ee166db0e4989f1ff88f
cydro <cmrnfaith@gmail.com>
2022-10-06 14:44:12 -0600

    fix rarity script

--- image-and-trait-generation/src/main.js
+++ image-and-trait-generation/src/main.js
@@ -140,7 +140,7 @@ const addAttributes = (_element) => {
   let selectedElement = _element.layer.selectedElement;
   attributesList.push({
     trait_type: _element.layer.name,
-    file_name: selectedElement.name,
+    value: selectedElement.name,
   });
 };
 
